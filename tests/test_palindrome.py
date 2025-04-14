743027be1e4d93bcfc17162b0e2b105e8a77a449
francomaldonado1 <f.maldonado@alumno.um.edu.ar>
2025-04-13 21:42:29 -0300

    Implementando test simples

---
+++ tests/test_palindrome.py
@@ -0,0 +1,17 @@
+import unittest
+from src.palindrome import is_palindrome
+
+class TestPalindrome(unittest.TestCase):  
+    def test_simple_palindromes(self):  
+        self.assertTrue(is_palindrome("madam"))  
+        self.assertTrue(is_palindrome("racecar"))  
+        self.assertTrue(is_palindrome("level"))  
+        self.assertTrue(is_palindrome("neuquen"))  
+        self.assertTrue(is_palindrome("rotor"))  
+        self.assertTrue(is_palindrome("deified"))  
+        self.assertTrue(is_palindrome("civic"))  
+        self.assertTrue(is_palindrome("refer"))  
+        self.assertTrue(is_palindrome("kayak"))  
+
+if __name__ == '__main__':  
+ unittest.main() 
